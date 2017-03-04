8823aa9d62c72e01593fd016a4acbc7711ac4b18
Timm Friebe <thekid@thekid.de>
2017-03-04 17:12:40 +0100

    QA: Use guard-style instead of indenting

--- src/main/php/web/Request.class.php
+++ src/main/php/web/Request.class.php
@@ -65,63 +65,62 @@ class Request {
   }
 
   /**
-   * Parses payload into parameters
+   * Parses payload into parameters and handles encoding (both cached)
    *
    * @see    http://www.w3.org/TR/html5/forms.html#application/x-www-form-urlencoded-encoding-algorithm
    * @return void
    */
   private function parse() {
-    if (null === $this->params) {
+    if (null !== $this->params) return;
 
-      // Merge parameters from URL and urlencoded payload.
-      $query= $this->uri->getQuery();
-      $type= new ContentType($this->header('Content-Type'));
-      if ($type->matches('application/x-www-form-urlencoded')) {
-        $query.= '&'.$this->input->read($this->header('Content-Length', -1));
+    // Merge parameters from URL and urlencoded payload.
+    $query= $this->uri->getQuery();
+    $type= new ContentType($this->header('Content-Type'));
+    if ($type->matches('application/x-www-form-urlencoded')) {
+      $query.= '&'.$this->input->read($this->header('Content-Length', -1));
+    }
+    parse_str($query, $this->params);
+
+    // Be liberal in what we accept and support "; charset=XXX" although the spec
+    // states this media type does not have parameters! Then, handle the special
+    // parameter named "_charset_" as per spec.
+    if ($this->encoding= $type->param('charset', null)) {
+      return;
+    } else if (isset($this->params['_charset_'])) {
+      $this->encoding= $this->params['_charset_'];
+      unset($this->params['_charset_']);
+      return;
+    }
+
+    // Otherwise, detect encoding.
+    $this->encoding= 'utf-8';
+
+    $offset= 0;
+    while (false !== ($p= strpos($query, '%', $offset))) {
+      $chr= hexdec($query{$p + 1}.$query{$p + 2});
+
+      if ($chr < 0x80) {                              // OK, same as ASCII
+        $offset= $p + 3;
+        continue;
+      } else if ($chr >= 0xc2 && $chr <= 0xdf) {      // 2 byte sequence
+        $sequence= [substr($query, $p + 4, 2)];
+        $offset= $p + 6;
+      } else if ($chr >= 0xe0 && $chr <= 0xef) {      // 3 byte sequence
+        $sequence= [substr($query, $p + 4, 2), substr($query, $p + 7, 2)];
+        $offset= $p + 9;
+      } else if ($chr >= 0xf0 && $chr <= 0xf4) {      // 4 byte sequence
+        $sequence= [substr($query, $p + 4, 2), substr($query, $p + 7, 2), substr($query, $p + 10, 2)];
+        $offset= $p + 12;
+      } else {
+        $this->encoding= 'iso-8859-1';
+        break;
       }
-      parse_str($query, $this->params);
 
-      // Be liberal in what we accept and support "; charset=XXX" although the spec
-      // states this media type does not have parameters! Then, handle the special
-      // parameter named "_charset_" as per spec.
-      if ($this->encoding= $type->param('charset', null)) {
-        return;
-      } else if (isset($this->params['_charset_'])) {
-        $this->encoding= $this->params['_charset_'];
-        unset($this->params['_charset_']);
-        return;
-      }
-
-      // Otherwise, detect encoding.
-      $this->encoding= 'utf-8';
-
-      $offset= 0;
-      while (false !== ($p= strpos($query, '%', $offset))) {
-        $chr= hexdec($query{$p + 1}.$query{$p + 2});
-
-        if ($chr < 0x80) {                              // OK, same as ASCII
-          $offset= $p + 3;
-          continue;
-        } else if ($chr >= 0xc2 && $chr <= 0xdf) {      // 2 byte sequence
-          $sequence= [substr($query, $p + 4, 2)];
-          $offset= $p + 6;
-        } else if ($chr >= 0xe0 && $chr <= 0xef) {      // 3 byte sequence
-          $sequence= [substr($query, $p + 4, 2), substr($query, $p + 7, 2)];
-          $offset= $p + 9;
-        } else if ($chr >= 0xf0 && $chr <= 0xf4) {      // 4 byte sequence
-          $sequence= [substr($query, $p + 4, 2), substr($query, $p + 7, 2), substr($query, $p + 10, 2)];
-          $offset= $p + 12;
-        } else {
+      foreach ($sequence as $bytes) {
+        $chr= hexdec($bytes);
+        if ($chr < 0x80 || $chr > 0xbf) {
           $this->encoding= 'iso-8859-1';
-          break;
-        }
-
-        foreach ($sequence as $bytes) {
-          $chr= hexdec($bytes);
-          if ($chr < 0x80 || $chr > 0xbf) {
-            $this->encoding= 'iso-8859-1';
-            break 2;
-          }
+          break 2;
         }
       }
     }
