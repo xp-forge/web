d4ef613cce2278509b9d01895f7e2a8a87c49e06
Timm Friebe <thekid@thekid.de>
2017-03-05 18:14:08 +0100

    QA: API docs

--- src/test/php/web/unittest/TestInput.class.php
+++ src/test/php/web/unittest/TestInput.class.php
@@ -3,6 +3,14 @@
 class TestInput implements \web\io\Input {
   private $method, $uri, $headers, $body;
 
+  /**
+   * Creates a new instance
+   *
+   * @param  string $method
+   * @param  string $uri
+   * @param  [:string] $headers
+   * @param  string $body
+   */
   public function __construct($method, $uri, $headers= [], $body= '') {
     $this->method= $method;
     $this->uri= $uri;
@@ -10,14 +18,24 @@ class TestInput implements \web\io\Input {
     $this->body= $body;
   }
 
+  /** @return string */
   public function scheme() { return 'http'; }
 
+  /** @return string */
   public function method() { return $this->method; }
 
+  /** @return string */
   public function uri() { return $this->uri; }
 
+  /** @return iterable */
   public function headers() { return $this->headers; }
 
+  /**
+   * Reads a given number of bytes
+   *
+   * @param  int $length Pass -1 to read all
+   * @return string
+   */
   public function read($length= -1) {
     if (-1 === $length) {
       $return= $this->body;
