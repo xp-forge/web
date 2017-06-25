3a7e6ae05b44e73f2876a38181c258680d4c9dd0
Timm Friebe <thekid@thekid.de>
2017-06-25 12:57:42 +0200

    Fix "xp web -r . -" for serving files from the current directory

--- src/main/php/xp/web/ServeDocumentRootStatically.class.php
+++ src/main/php/xp/web/ServeDocumentRootStatically.class.php
@@ -1,12 +1,12 @@
 <?php namespace xp\web;
 
 use web\Application;
-use web\handler\FilesIn;
+use web\handler\FilesFrom;
 
 class ServeDocumentRootStatically extends Application {
 
   /** @return web.Routing|[:var] */
   public function routes() {
-    return ['/' => new FilesIn($this->environment->docroot())];
+    return ['/' => new FilesFrom($this->environment->docroot())];
   }
 }
