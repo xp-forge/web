1cde73159bb4951e4c9fdf82ad682a2072eb97ff
Timm Friebe <thekid@thekid.de>
2017-07-04 23:22:45 +0200

    Also accept causes as string

--- src/main/php/web/InternalServerError.class.php
+++ src/main/php/web/InternalServerError.class.php
@@ -1,13 +1,19 @@
 <?php namespace web;
 
+use lang\Throwable;
+
 class InternalServerError extends Error {
 
   /**
    * Creates a new internal server error
    *
-   * @param  php.Throwable $cause
+   * @param  php.Throwable|string $cause
    */
   public function __construct($cause) {
-    parent::__construct(500, $cause->getMessage(), $cause);
+    if ($cause instanceof Throwable) {
+      parent::__construct(500, $cause->getMessage(), $cause);
+    } else {
+      parent::__construct(500, $cause);
+    }
   }
 }
