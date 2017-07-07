fa1fdb924fdd9f7b0b8279f9b65165c71b52f0ad
Timm Friebe <thekid@thekid.de>
2017-07-07 02:14:41 +0200

    Add prefork-server

---
+++ src/main/php/xp/web/Prefork.class.php
@@ -0,0 +1,23 @@
+<?php namespace xp\web;
+
+use peer\server\PreforkingServer;
+
+/**
+ * Serves requests from a perforking server
+ *
+ * @ext   pcntl
+ * @see   xp://peer.server.PreforkingServer
+ */
+class Prefork extends Standalone {
+
+  /**
+   * Creates a new instance
+   *
+   * @param  string $host
+   * @param  int $port
+   * @param  int $children How many children to initially fork, defaults to 10
+   */
+  public function __construct($host, $port, $children= 10) {
+    parent::__construct(new PreforkingServer($host, $port, $children), 'http://'.$host.':'.$port.'/');
+  }
+}
