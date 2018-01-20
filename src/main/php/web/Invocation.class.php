42b740b600b2f97a157614586de79f6b497ed0ab
Timm Friebe <thekid@thekid.de>
2018-01-20 18:32:46 +0100

    QA: Apidocs

--- src/main/php/web/Invocation.class.php
+++
@@ -1,8 +0,0 @@
-<?php namespace web;
-
-class Invocation {
-
-  public function proceed($request, $response) {
-    $this->target->route($request, $response);
-  }
-}
--- src/main/php/web/filters/Invocation.class.php
+++ src/main/php/web/filters/Invocation.class.php
@@ -1,14 +1,28 @@
 <?php namespace web\filters;
 
 class Invocation {
+  private $routing, $filters, $offset, $length;
 
-  public function __construct($routing, $filters) {
+  /**
+   * Create a new Invocation
+   *
+   * @param  web.Routing $routing
+   * @param  web.Filter[] $filters
+   */
+  public function __construct($routing, $filters= []) {
     $this->routing= $routing;
     $this->filters= $filters;
     $this->offset= 0;
     $this->length= sizeof($filters);
   }
 
+  /**
+   * Proceed with the invocation
+   *
+   * @param  web.Request $request
+   * @param  web.Response $response
+   * @return void
+   */
   public function proceed($request, $response) {
     if ($this->offset < $this->length) {
       $this->filters[$this->offset++]->filter($request, $response, $this);
