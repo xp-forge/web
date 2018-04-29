e42eb4d49f7eb2c5404a99c158552a08a57c83b5
Timm Friebe <thekid@thekid.de>
2018-04-29 16:19:50 +0200

    Allow patterns inside segments
    E.g. `/articles/{category}/{id:[0-9]+}`

--- src/test/php/web/unittest/routing/SegmentsTest.class.php
+++ src/test/php/web/unittest/routing/SegmentsTest.class.php
@@ -17,4 +17,13 @@ class SegmentsTest extends \unittest\TestCase {
   public function matches($path, $expected) {
     $this->assertEquals($expected, (new Segments('/users/{id}'))->matches(new Request(new TestInput('GET', $path))));
   }
+
+  #[@test, @values([
+  #  ['/users/1549', ['id' => '1549']],
+  #  ['/users/1549/', ['id' => '1549']],
+  #  ['/users/friebe', null]
+  #])]
+  public function matches_with_pattern($path, $expected) {
+    $this->assertEquals($expected, (new Segments('/users/{id:[0-9]+}'))->matches(new Request(new TestInput('GET', $path))));
+  }
 }
