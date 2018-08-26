c9896e5c5a021c8cd72f87723c85fd3b69b3cc9c
Timm Friebe <thekid@thekid.de>
2018-08-26 21:27:34 +0200

    Add tests for a variety of chunked input data

--- src/test/php/web/unittest/io/ReadChunksTest.class.php
+++ src/test/php/web/unittest/io/ReadChunksTest.class.php
@@ -1,8 +1,8 @@
 <?php namespace web\unittest\io;
 
+use lang\FormatException;
 use web\io\ReadChunks;
 use web\io\TestInput;
-use lang\FormatException;
 
 class ReadChunksTest extends \unittest\TestCase {
 
@@ -34,14 +34,19 @@ class ReadChunksTest extends \unittest\TestCase {
     $this->assertEquals('Test', $fixture->read());
   }
 
-  #[@test]
-  public function while_loop() {
-    $fixture= new ReadChunks(new TestInput('GET', '/', [], "4\r\nTest\r\n0\r\n\r\n"));
+  #[@test, @values([
+  #  ["0\r\n\r\n", []],
+  #  ["4\r\nTest\r\n0\r\n\r\n", ['Test']],
+  #  ["e\r\n{\"name\":\"PHP\"}\r\n0\r\n\r\n", ['{"name":"PHP"}']],
+  #  ["F\r\n{\"name\":\"JSON\"}\r\n0\r\n\r\n", ['{"name":"JSON"}']],
+  #])]
+  public function while_loop($chunked, $expected) {
+    $fixture= new ReadChunks(new TestInput('GET', '/', [], $chunked));
     $r= [];
     while ($fixture->available()) {
       $r[]= $fixture->read();
     }
-    $this->assertEquals(['Test'], $r);
+    $this->assertEquals($expected, $r);
   }
 
   #[@test]
@@ -52,5 +57,35 @@ class ReadChunksTest extends \unittest\TestCase {
       $fixture->read();
     }
     $this->assertEquals('', $input->read(-1));
+  }
+
+  #[@test]
+  public function read_invocations() {
+    $input= newinstance(TestInput::class, ['GET', '/', [], "4\r\nTest\r\n2\r\ned\r\n0\r\n\r\n"], [
+      'invocations' => [],
+      'read' => function($length= -1) {
+        $result= parent::read($length);
+        $this->invocations[]= 'read:'.$length.':"'.addcslashes($result, "\r\n").'"';
+        return $result;
+      },
+      'readLine' => function() {
+        $result= parent::readLine();
+        $this->invocations[]= 'readLine:"'.addcslashes($result, "\r\n").'"';
+        return $result;
+      }
+    ]);
+    $fixture= new ReadChunks($input);
+    while ($fixture->available()) {
+      $fixture->read();
+    }
+
+    $this->assertEquals(
+      [
+        'readLine:"4\r"', 'read:4:"Test"', 'readLine:"\r"',
+        'readLine:"2\r"', 'read:2:"ed"', 'readLine:"\r"',
+        'readLine:"0\r"', 'readLine:"\r"'
+      ],
+      $input->invocations
+    );
   }
 }
