ef5df47d39e815b0b215ed2b4b3dc41829148bc6
Timm Friebe <thekid@thekid.de>
2018-08-26 22:01:26 +0200

    Add tests for chunked input and output

--- src/test/php/web/unittest/HttpProtocolTest.class.php
+++ src/test/php/web/unittest/HttpProtocolTest.class.php
@@ -1,5 +1,6 @@
 <?php namespace web\unittest;
 
+use io\streams\Streams;
 use unittest\TestCase;
 use web\Application;
 use web\Environment;
@@ -89,6 +90,61 @@ class HttpProtocolTest extends TestCase {
       "Date: ".gmdate('D, d M Y H:i:s T')."\r\n".
       "Content-Length: 0\r\n".
       "\r\n",
+      implode('', $c->out)
+    );
+  }
+
+  #[@test]
+  public function handles_chunked_transfer_input() {
+    $echo= function($req, $res) { $res->send(Streams::readAll($req->stream()), 'text/plain'); };
+    $p= new HttpProtocol($this->application($echo), $this->log);
+
+    $c= new Channel([
+      "POST / HTTP/1.1\r\nContent-Type: text/plain\r\nTransfer-Encoding: chunked\r\n\r\n",
+      "4\r\nTest\r\n0\r\n\r\n"
+    ]);
+    $p->handleData($c);
+
+    $this->assertEquals(
+      "HTTP/1.1 200 OK\r\n".
+      "Date: ".gmdate('D, d M Y H:i:s T')."\r\n".
+      "Content-Type: text/plain\r\n".
+      "Content-Length: 4\r\n".
+      "\r\nTest",
+      implode('', $c->out)
+    );
+  }
+
+  #[@test]
+  public function buffers_and_sets_content_length_for_http10() {
+    $echo= function($req, $res) { with ($res->stream(), function($s) { $s->write('Test'); }); };
+    $p= new HttpProtocol($this->application($echo), $this->log);
+
+    $c= new Channel(["GET / HTTP/1.0\r\n\r\n"]);
+    $p->handleData($c);
+
+    $this->assertEquals(
+      "HTTP/1.0 200 OK\r\n".
+      "Date: ".gmdate('D, d M Y H:i:s T')."\r\n".
+      "Content-Length: 4\r\n".
+      "\r\nTest",
+      implode('', $c->out)
+    );
+  }
+
+  #[@test]
+  public function produces_chunked_transfer_output_for_http11() {
+    $echo= function($req, $res) { with ($res->stream(), function($s) { $s->write('Test'); }); };
+    $p= new HttpProtocol($this->application($echo), $this->log);
+
+    $c= new Channel(["GET / HTTP/1.1\r\n\r\n"]);
+    $p->handleData($c);
+
+    $this->assertEquals(
+      "HTTP/1.1 200 OK\r\n".
+      "Date: ".gmdate('D, d M Y H:i:s T')."\r\n".
+      "Transfer-Encoding: chunked\r\n".
+      "\r\n4\r\nTest\r\n0\r\n\r\n",
       implode('', $c->out)
     );
   }
