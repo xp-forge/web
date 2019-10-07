dac2c455ee6edda5e33d9af9d3220e1f845bafd3
Timm Friebe <thekid@thekid.de>
2019-10-07 18:06:09 +0200

    Also accept filter functions in constructor

--- src/main/php/web/Filters.class.php
+++ src/main/php/web/Filters.class.php
@@ -3,18 +3,32 @@
 use web\filters\Invocation;
 
 class Filters implements Handler {
+  private $filters= [];
 
   /**
    * Creates a new instance
    *
-   * @param  web.Filter[] $filters
+   * @param  (web.Filter|function(web.Request, web.Response, web.filters.Invocation)[] $filters
    * @param  [:web.Routing]|web.Routing $routing
    */
   public function __construct($filters, $routing) {
-    $this->filters= $filters;
+    foreach ($filters as $filter) {
+      if ($filter instanceof Filter) {
+        $this->filters[]= $filter;
+      } else {
+        $this->filters[]= newinstance(Filter::class, [], ['filter' => $filter]);
+      }
+    }
     $this->routing= Routing::cast($routing);
   }
 
+  /**
+   * Filter request
+   *
+   * @param  web.Request $request
+   * @param  web.Response $response
+   * @param  var
+   */
   public function handle($request, $response) {
     return (new Invocation($this->routing, $this->filters))->proceed($request, $response);
   }
