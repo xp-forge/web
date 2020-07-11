3a0c8bb07d84df83a93f84dd10eb089001d473a4
Timm Friebe <thekid@thekid.de>
2020-07-11 18:09:03 +0200

    Do not start reading from stream until I/O methods are actually called

--- src/main/php/web/io/ReadChunks.class.php
+++ src/main/php/web/io/ReadChunks.class.php
@@ -1,6 +1,7 @@
 <?php namespace web\io;
 
-use lang\FormatException;
+use io\IOException;
+use io\streams\InputStream;
 
 /**
  * Reads chunked transfer encoding 
@@ -8,23 +9,34 @@ use lang\FormatException;
  * @test  xp://web.unittest.io.ReadChunksTest
  * @see   https://tools.ietf.org/html/rfc7230#section-4.1
  */
-class ReadChunks implements \io\streams\InputStream {
-  private $input, $remaining, $length, $buffer;
+class ReadChunks implements InputStream {
+  private $input;
+  private $remaining= null;
+  private $buffer= '';
 
   /**
    * Scans a chunk, populating length and buffer
    *
    * @return void
+   * @throws io.IOException for chunked format errors
    */
   private function scan() {
     $size= $this->input->readLine();
-    if (1 !== sscanf($size, "%x", $this->length)) {
-      throw new FormatException('No chunk segment present');
+
+    if (1 !== sscanf($size, '%x', $l)) {
+      throw new IOException('No chunk segment present (`'.addcslashes($size, "\0..\37").'`)');
     }
 
-    $this->remaining= $this->length;
-    $this->buffer= 0 === $this->length ? '' : $this->input->read($this->length);
+    // Chunk with 0 length indicates EOF
+    if ($l > 0) {
+      $this->buffer.= $this->input->read($l);
+      $this->remaining+= $l;
+    } else {
+      $this->remaining= 0;
+    }
+
     $this->input->readLine();
+    return $this->remaining;
   }
 
   /**
@@ -34,7 +46,26 @@ class ReadChunks implements \io\streams\InputStream {
    */
   public function __construct(Input $input) {
     $this->input= $input;
-    $this->scan();
+  }
+
+  /**
+   * Reads next line terminated by `<CRLF>`
+   *
+   * @return string
+   */
+  public function line() {
+    while (false === ($p= strpos($this->buffer, "\r\n"))) {
+      if (0 === $this->scan()) return $this->buffer;  // EOF
+    }
+
+    $chunk= substr($this->buffer, 0, $p);
+    $length= strlen($chunk) + 2;
+    $this->buffer= substr($this->buffer, $p + 2);
+
+    if (0 === ($this->remaining-= $length)) {
+      $this->scan();
+    }
+    return $chunk;
   }
 
   /**
@@ -44,9 +75,12 @@ class ReadChunks implements \io\streams\InputStream {
    * @return string
    */
   public function read($limit= 8192) {
-    $chunk= substr($this->buffer, 0, min($limit, $this->remaining));
+    $remaining= $this->remaining ?? $this->scan();
+
+    $chunk= substr($this->buffer, 0, min($limit, $remaining));
     $length= strlen($chunk);
     $this->buffer= substr($this->buffer, $length);
+
     if (0 === ($this->remaining-= $length)) {
       $this->scan();
     }
@@ -54,7 +88,9 @@ class ReadChunks implements \io\streams\InputStream {
   }
 
   /** @return int */
-  public function available() { return $this->length; }
+  public function available() {
+    return $this->remaining ?? $this->scan();
+  }
 
   /** @return void */
   public function close() { /* NOOP */  }
--- src/test/php/web/unittest/io/ReadChunksTest.class.php
+++ src/test/php/web/unittest/io/ReadChunksTest.class.php
@@ -1,46 +1,85 @@
 <?php namespace web\unittest\io;
 
-use lang\FormatException;
-use web\io\{ReadChunks, TestInput};
+use io\IOException;
+use unittest\TestCase;
+use web\io\ReadChunks;
+use web\io\TestInput;
+use web\unittest\Chunking;
 
-class ReadChunksTest extends \unittest\TestCase {
+class ReadChunksTest extends TestCase {
+  use Chunking;
+
+  /**
+   * Creates an input instance
+   *
+   * @param  string $bytes
+   * @return web.io.Input
+   */
+  private function input($bytes) {
+    return new TestInput('POST', '/', self::$CHUNKED, $bytes);
+  }
 
   #[@test]
   public function can_create() {
-    new ReadChunks(new TestInput('GET', '/', [], "0\r\n\r\n"));
+    new ReadChunks($this->input("0\r\n\r\n"));
   }
 
-  #[@test, @expect(FormatException::class)]
-  public function raises_exception_when_non_chunked_data_appears() {
-    new ReadChunks(new TestInput('GET', '/', [], ''));
+  #[@test, @expect(IOException::class)]
+  public function raises_exception_from_read_when_non_chunked_data_appears() {
+    $fixture= new ReadChunks($this->input(''));
+    $fixture->read();
+  }
+
+  #[@test, @expect(IOException::class)]
+  public function raises_exception_from_available_when_non_chunked_data_appears() {
+    $fixture= new ReadChunks($this->input(''));
+    $fixture->available();
+  }
+
+  #[@test]
+  public function does_not_raise_exception_until_read_or_available_accessed() {
+    new ReadChunks($this->input(''));
   }
 
   #[@test]
   public function available() {
-    $fixture= new ReadChunks(new TestInput('GET', '/', [], "4\r\nTest\r\n0\r\n\r\n"));
+    $fixture= new ReadChunks($this->input("4\r\nTest\r\n0\r\n\r\n"));
     $this->assertEquals(4, $fixture->available());
+  }
+
+  #[@test, @values([
+  #  [2, 'Te', 2],
+  #  [4, 'Test', 6],
+  #  [6, 'Test', 6],
+  #])]
+  public function available_after_reading($length, $read, $remaining) {
+    $fixture= new ReadChunks($this->input("4\r\nTest\r\n6\r\nTested\r\n0\r\n\r\n"));
+    $this->assertEquals($read, $fixture->read($length));
+    $this->assertEquals($remaining, $fixture->available());
   }
 
   #[@test]
   public function available_last_chunk() {
-    $fixture= new ReadChunks(new TestInput('GET', '/', [], "0\r\n\r\n"));
+    $fixture= new ReadChunks($this->input("0\r\n\r\n"));
     $this->assertEquals(0, $fixture->available());
   }
 
   #[@test]
   public function read() {
-    $fixture= new ReadChunks(new TestInput('GET', '/', [], "4\r\nTest\r\n0\r\n\r\n"));
+    $fixture= new ReadChunks($this->input("4\r\nTest\r\n0\r\n\r\n"));
     $this->assertEquals('Test', $fixture->read());
   }
 
   #[@test, @values([
   #  ["0\r\n\r\n", []],
   #  ["4\r\nTest\r\n0\r\n\r\n", ['Test']],
+  #  ["4\r\nTest\r\n2\r\nOK\r\n0\r\n\r\n", ['Test', 'OK']],
+  #  ["6\r\nTest\r\n\r\n0\r\n\r\n", ["Test\r\n"]],
   #  ["e\r\n{\"name\":\"PHP\"}\r\n0\r\n\r\n", ['{"name":"PHP"}']],
   #  ["F\r\n{\"name\":\"JSON\"}\r\n0\r\n\r\n", ['{"name":"JSON"}']],
   #])]
-  public function while_loop($chunked, $expected) {
-    $fixture= new ReadChunks(new TestInput('GET', '/', [], $chunked));
+  public function chunks($chunked, $expected) {
+    $fixture= new ReadChunks($this->input($chunked));
     $r= [];
     while ($fixture->available()) {
       $r[]= $fixture->read();
@@ -50,7 +89,7 @@ class ReadChunksTest extends \unittest\TestCase {
 
   #[@test]
   public function read_until_end() {
-    $input= new TestInput('GET', '/', [], "4\r\nTest\r\n0\r\n\r\n");
+    $input= $this->input("4\r\nTest\r\n0\r\n\r\n");
     $fixture= new ReadChunks($input);
     while ($fixture->available()) {
       $fixture->read();
@@ -60,21 +99,19 @@ class ReadChunksTest extends \unittest\TestCase {
 
   #[@test]
   public function read_invocations() {
-    $input= new class('GET', '/', [], "4\r\nTest\r\n2\r\ned\r\n0\r\n\r\n") extends TestInput {
-      public $invocations= [];
-
-      public function read($length= -1) {
+    $input= newinstance(TestInput::class, ['POST', '/', self::$CHUNKED, "4\r\nTest\r\n2\r\ned\r\n0\r\n\r\n"], [
+      'invocations' => [],
+      'read' => function($length= -1) {
         $result= parent::read($length);
         $this->invocations[]= 'read:'.$length.':"'.addcslashes($result, "\r\n").'"';
         return $result;
-      }
-
-      public function readLine() {
+      },
+      'readLine' => function() {
         $result= parent::readLine();
         $this->invocations[]= 'readLine:"'.addcslashes($result, "\r\n").'"';
         return $result;
       }
-    };
+    ]);
     $fixture= new ReadChunks($input);
     while ($fixture->available()) {
       $fixture->read();
@@ -82,9 +119,9 @@ class ReadChunksTest extends \unittest\TestCase {
 
     $this->assertEquals(
       [
-        'readLine:"4\r"', 'read:4:"Test"', 'readLine:"\r"',
-        'readLine:"2\r"', 'read:2:"ed"', 'readLine:"\r"',
-        'readLine:"0\r"', 'readLine:"\r"'
+        'readLine:"4"', 'read:4:"Test"', 'readLine:""',
+        'readLine:"2"', 'read:2:"ed"', 'readLine:""',
+        'readLine:"0"', 'readLine:""'
       ],
       $input->invocations
     );
