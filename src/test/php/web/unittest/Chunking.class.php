b95ab6a8cef1c2527aca1a0687b2ebff5aee3ef7
Timm Friebe <thekid@thekid.de>
2020-07-11 18:23:57 +0200

    Extract utilities for testing chunked transfer encoding

---
+++ src/test/php/web/unittest/Chunking.class.php
@@ -0,0 +1,27 @@
+<?php namespace web\unittest;
+
+/**
+ * Chunked transfer encoding
+ *
+ * @see  https://en.wikipedia.org/wiki/Chunked_transfer_encoding
+ * @see  https://tools.ietf.org/html/rfc7230#section-4.1
+ */
+trait Chunking {
+  private static $CHUNKED = ['Transfer-Encoding' => 'chunked'];
+
+  /**
+   * Creates a chunked payload
+   *
+   * @param  string $payload
+   * @param  int $length
+   * @return string
+   */
+  private function chunked($payload, $length= 0xff) {
+    $chunked= '';
+    for ($i= 0; $i < strlen($payload); $i+= $length) {
+      $chunk= substr($payload, $i, $length);
+      $chunked.= sprintf("%x\r\n%s\r\n", strlen($chunk), $chunk);
+    }
+    return $chunked."0\r\n\r\n";
+  }
+}
