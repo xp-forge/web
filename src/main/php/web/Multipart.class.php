d928f1b1c582e4656690c1c1422f2e47cc5e30cd
Timm Friebe <thekid@thekid.de>
2020-08-30 16:10:10 +0200

    Parse multipart payloads up until the first file
    This is done to make parameters passed along with multipart payloads accessible
    even before processing all uploaded files. The usecase is to pass a CSRF token
    as a hidden field, and have that validated before processing the files, something
    that e.g. xp-forge/frontend does automatically.
    
    See also PR #65

--- src/main/php/web/Multipart.class.php
+++ src/main/php/web/Multipart.class.php
@@ -12,34 +12,68 @@ use web\io\Part;
 class Multipart {
   const MIME = 'multipart/form-data';
 
-  private $request, $boundary;
+  private $parts, $params;
+  private $peeked= null;
 
   /**
    * Creates a new Multipart instance
    *
-   * @param  web.Request $request
-   * @param  web.Parameterized $type
+   * @param  iterable $parts
+   * @param  [:var] $params
    */
-  public function __construct($request, $type) {
-    $this->request= $request;
-    $this->boundary= $type->param('boundary');
+  public function __construct($parts, &$params) {
+    if ($parts instanceof \Iterator) {
+      $this->parts= $parts;
+    } else if ($parts instanceof \IteratorAggregate) {
+      $this->parts= $parts->getIterator();
+    } else {
+      $this->parts= new \ArrayIterator($parts);
+    }
+    $this->params= &$params;
   }
 
-  /** @return string */
-  public function boundary() { return $this->boundary; }
- 
+  /**
+   * Returns all parameters *before* files, parsing the request body until
+   * that position.
+   *
+   * @return web.io.Param[]
+   */
+  public function peek() {
+    if (null !== $this->peeked) return $this->peeked;
+
+    $this->peeked= [];
+    while ($this->parts->valid()) {
+      $part= $this->parts->current();
+      if (Part::PARAM !== $part->kind()) break;
+
+      parse_str($this->parts->key().'='.$part->value(), $params);
+      $this->params= array_merge_recursive($this->params, $params);
+      $this->parts->next();
+      $this->peeked[]= $part;
+    }
+    return $this->peeked;
+  }
+
   /**
    * Returns all parts - files and parameters.
    *
    * @return iterable
    */
   public function parts() {
-    foreach ($this->request->input()->parts($this->boundary) as $name => $part) {
+    if ($this->peeked) foreach ($this->peeked as $param) {
+      yield $param->name() => $param;
+    }
+    $this->peeked= [];
+
+    while ($this->parts->valid()) {
+      $name= $this->parts->key();
+      $part= $this->parts->current();
       if (Part::PARAM === $part->kind()) {
         parse_str($name.'='.$part->value(), $params);
-        $this->request->add($params);
+        $this->params= array_merge_recursive($this->params, $params);
       }
       yield $name => $part;
+      $this->parts->next();
     }
   }
 
@@ -49,14 +83,19 @@ class Multipart {
    * @return iterable
    */
   public function files() {
-    foreach ($this->request->input()->parts($this->boundary) as $name => $part) {
+    $this->peeked= [];
+
+    while ($this->parts->valid()) {
+      $name= $this->parts->key();
+      $part= $this->parts->current();
       $kind= $part->kind();
       if (Part::PARAM === $kind) {
         parse_str($name.'='.$part->value(), $params);
-        $this->request->add($params);
+        $this->params= array_merge_recursive($this->params, $params);
       } else if (Part::FILE === $kind) {
         yield $part->name() => $part;
       }
+      $this->parts->next();
     }
   }
 }
