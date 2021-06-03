6cd9b2ca6f510820ee00546e72210646e39686e7
Timm Friebe <thekid@thekid.de>
2021-06-03 15:28:23 +0200

    Remove error cleanup, fixed with xp-framework/networking@da2258c571d46b95c37f9de8838b05ff0ed7660d

--- src/main/php/xp/web/srv/HttpProtocol.class.php
+++ src/main/php/xp/web/srv/HttpProtocol.class.php
@@ -38,7 +38,6 @@ class HttpProtocol implements ServerProtocol {
     // Unwind generators returned from handleData() to guarantee their complete
     // execution.
     if (class_exists(AsyncServer::class, true)) {
-      \xp::gc();
       return new self($application, $logging);
     } else {
       return new class($application, $logging) extends HttpProtocol {
