2c55dbe827e33f2466429c3498e8bd11eba9e0d0
Timm Friebe <thekid@thekid.de>
2021-06-14 20:01:20 +0200

    Convert to Assert DSL, fixing PHP 8+

--- src/test/php/web/unittest/filters/BehindProxyTest.class.php
+++ src/test/php/web/unittest/filters/BehindProxyTest.class.php
@@ -1,53 +1,50 @@
 <?php namespace web\unittest\filters;
 
-use unittest\TestCase;
-use web\filters\BehindProxy;
-use web\filters\Invocation;
-use web\io\TestInput;
-use web\io\TestOutput;
-use web\Request;
-use web\Response;
+use unittest\{Assert, Test, Values};
+use web\filters\{BehindProxy, Invocation};
+use web\io\{TestInput, TestOutput};
+use web\{Request, Response};
 
-class BehindProxyTest extends TestCase {
+class BehindProxyTest {
 
-  #[@test]
+  #[Test]
   public function can_create() {
     new BehindProxy(['http://remote' => '/']);
   }
 
-  #[@test, @values(['/', '/path', '/path/', '/path/index.html'])]
+  #[Test, Values(['/', '/path', '/path/', '/path/index.html'])]
   public function rewrite_subdomain_to_base($path) {
     $request= new Request(new TestInput('GET', $path));
     $fixture= new BehindProxy(['https://service.example.com/' => '/']);
     $fixture->filter($request, new Response(new TestOutput()), new Invocation(function($req, $res) { }));
 
-    $this->assertEquals('https://service.example.com'.$path, (string)$request->uri());
+    Assert::equals('https://service.example.com'.$path, (string)$request->uri());
   }
 
-  #[@test, @values(['/', '/path', '/path/', '/path/index.html'])]
+  #[Test, Values(['/', '/path', '/path/', '/path/index.html'])]
   public function rewrite_path_to_base($path) {
     $request= new Request(new TestInput('GET', $path));
     $fixture= new BehindProxy(['https://example.com/service/' => '/']);
     $fixture->filter($request, new Response(new TestOutput()), new Invocation(function($req, $res) { }));
 
-    $this->assertEquals('https://example.com/service'.$path, (string)$request->uri());
+    Assert::equals('https://example.com/service'.$path, (string)$request->uri());
   }
 
-  #[@test, @values(['/', '/path', '/path/', '/path/index.html'])]
+  #[Test, Values(['/', '/path', '/path/', '/path/index.html'])]
   public function rewrite_subdomain_to_path($path) {
     $request= new Request(new TestInput('GET', '/service'.$path));
     $fixture= new BehindProxy(['https://service.example.com/' => '/service']);
     $fixture->filter($request, new Response(new TestOutput()), new Invocation(function($req, $res) { }));
 
-    $this->assertEquals('https://service.example.com'.$path, (string)$request->uri());
+    Assert::equals('https://service.example.com'.$path, (string)$request->uri());
   }
 
-  #[@test, @values(['/', '/path', '/path/', '/path/index.html'])]
+  #[Test, Values(['/', '/path', '/path/', '/path/index.html'])]
   public function rewrite_path_to_path($path) {
     $request= new Request(new TestInput('GET', '/service'.$path));
     $fixture= new BehindProxy(['https://example.com/service/' => '/service']);
     $fixture->filter($request, new Response(new TestOutput()), new Invocation(function($req, $res) { }));
 
-    $this->assertEquals('https://example.com/service'.$path, (string)$request->uri());
+    Assert::equals('https://example.com/service'.$path, (string)$request->uri());
   }
 }
