fe4af044c1bfca385be9a0c302d7339c5c6f932f
Timm Friebe <thekid@thekid.de>
2021-06-14 22:22:35 +0200

    Add tests for web.InternalServerError class

---
+++ src/test/php/web/unittest/InternalServerErrorTest.class.php
@@ -0,0 +1,19 @@
+<?php namespace web\unittest;
+
+use lang\IllegalArgumentException;
+use unittest\{Assert, Test};
+use web\InternalServerError;
+
+class InternalServerErrorTest {
+
+  #[Test]
+  public function can_create() {
+    new InternalServerError(new IllegalArgumentException('Test'));
+  }
+
+  #[Test]
+  public function uses_error_500() {
+    $e= new InternalServerError(new IllegalArgumentException('Test'));
+    Assert::equals(500, $e->status());
+  }
+}
