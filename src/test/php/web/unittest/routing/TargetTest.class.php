1b21cd9ecefd9fd5642c9cfc260e22ca42b9bd8f
Timm Friebe <thekid@thekid.de>
2021-06-15 18:28:31 +0200

    Test custom route matcher

--- src/test/php/web/unittest/routing/TargetTest.class.php
+++ src/test/php/web/unittest/routing/TargetTest.class.php
@@ -3,7 +3,7 @@
 use unittest\{Test, Values, TestCase};
 use web\Request;
 use web\io\TestInput;
-use web\routing\Target;
+use web\routing\{Target, RouteMatch};
 
 class TargetTest extends TestCase {
 
@@ -20,5 +20,13 @@ class TargetTest extends TestCase {
   #[Test, Values([['GET', '/test', true], ['GET', '/test/', true], ['GET', '/test/the/west', true], ['GET', '/test.html', false], ['GET', '/TEST', false], ['GET', '/', false], ['POST', '/test', false], ['POST', '/', false]])]
   public function method_and_path($method, $path, $expected) {
     $this->assertEquals($expected, (new Target('GET', '/test'))->matches(new Request(new TestInput($method, $path))));
+  }
+
+  #[Test, Values([[['X-Match' => 'yes'], true], [[], false]])]
+  public function on_headers($headers, $expected) {
+    $match= new class() implements RouteMatch {
+      public function matches($request) { return null !== $request->header('X-Match'); }
+    };
+    $this->assertEquals($expected, (new Target('GET', $match))->matches(new Request(new TestInput('GET', '/', $headers))));
   }
 }
