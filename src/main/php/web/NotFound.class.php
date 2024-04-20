ccf914ac974683dc80f4a5c32b394dae7a60ccc8
Timm Friebe <thekid@thekid.de>
2024-04-20 11:59:14 +0200

    QA: Add apidoc

--- src/main/php/web/NotFound.class.php
+++ src/main/php/web/NotFound.class.php
@@ -3,6 +3,12 @@
 class NotFound extends Error {
   public $path;
 
+  /**
+   * Creates a "Not Found" error
+   *
+   * @param  string $path
+   * @param  ?lang.Throwable $cause
+   */
   public function __construct($path, $cause= null) {
     parent::__construct(404, 'Cannot find '.$path, $cause);
     $this->path= $path;
