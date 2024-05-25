610342db7dd4ad216ea675fa31037f52c7c09703
Timm Friebe <thekid@thekid.de>
2024-05-25 17:00:10 +0200

    QA: No longer use protocol for referring to test classes

--- src/main/php/web/Headers.class.php
+++ src/main/php/web/Headers.class.php
@@ -8,7 +8,7 @@ use util\Date;
  *
  * @see   https://en.wikipedia.org/wiki/List_of_HTTP_header_fields
  * @see   https://en.wikipedia.org/wiki/Content_negotiation
- * @test  xp://web.unittest.HeadersTest
+ * @test  web.unittest.HeadersTest
  */
 abstract class Headers {
 
