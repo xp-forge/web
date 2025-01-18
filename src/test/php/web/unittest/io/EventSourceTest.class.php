0ea9a1097383eaa11c9137d373951c140df71a23
Timm Friebe <thekid@thekid.de>
2025-01-18 10:49:01 +0100

    Simplify test, add new variation

--- src/test/php/web/unittest/io/EventSourceTest.class.php
+++ src/test/php/web/unittest/io/EventSourceTest.class.php
@@ -1,15 +1,10 @@
 <?php namespace web\unittest\io;
 
-use io\streams\{InputStream, MemoryInputStream};
+use io\streams\MemoryInputStream;
 use test\{Assert, Test, Values};
 use web\io\EventSource;
 
 class EventSourceTest {
-
-  /** Returns an input stream from the given lines */
-  private function stream(array $lines): InputStream {
-    return new MemoryInputStream(implode("\n", $lines));
-  }
 
   /** @return iterable */
   private function inputs() {
@@ -17,19 +12,21 @@ class EventSourceTest {
     yield [[''], []];
     yield [['data: One'], [[null => 'One']]];
     yield [['', 'data: One'], [[null => 'One']]];
+    yield [['data: One', ''], [[null => 'One']]];
     yield [['data: One', '', 'data: Two'], [[null => 'One'], [null => 'Two']]];
     yield [['event: test', 'data: One'], [['test' => 'One']]];
     yield [['event: test', 'data: One', '', 'data: Two'], [['test' => 'One'], [null => 'Two']]];
+    yield [['event: one', 'data: 1', '', 'event: two', 'data: 2'], [['one' => '1'], ['two' => '2']]];
   }
 
   #[Test]
   public function can_create() {
-    new EventSource($this->stream([]));
+    new EventSource(new MemoryInputStream(''));
   }
 
   #[Test, Values(from: 'inputs')]
   public function events($lines, $expected) {
-    $events= new EventSource($this->stream($lines));
+    $events= new EventSource(new MemoryInputStream(implode("\n", $lines)));
     $actual= [];
     foreach ($events as $type => $event) {
       $actual[]= [$type => $event];
