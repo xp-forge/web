3e4a6b0c9251fb940f2239711eb5971fee408ef2
Timm Friebe <thekid@thekid.de>
2025-01-19 11:56:31 +0100

    Test translating text and binary messages to SSE

--- src/test/php/web/unittest/handler/WebSocketTest.class.php
+++ src/test/php/web/unittest/handler/WebSocketTest.class.php
@@ -1,6 +1,7 @@
 <?php namespace web\unittest\handler;
 
 use test\{Assert, Test};
+use util\Bytes;
 use web\handler\WebSocket;
 use web\io\{TestInput, TestOutput};
 use web\{Request, Response};
@@ -10,7 +11,14 @@ class WebSocketTest {
   /** Handles a request and returns the response generated from the handler */
   private function handle(Request $request): Response {
     $response= new Response(new TestOutput());
-    (new WebSocket(function($conn, $payload) { }))->handle($request, $response)->next();
+    $echo= function($conn, $payload) {
+      if ($payload instanceof Bytes) {
+        $conn->send(new Bytes("\x08\x15{$payload}"));
+      } else {
+        $conn->send('Re: '.$payload);
+      }
+    };
+    (new WebSocket($echo))->handle($request, $response)->next();
     return $response;
   }
 
@@ -27,6 +35,32 @@ class WebSocketTest {
     ])));
     Assert::equals(101, $response->status());
     Assert::equals('tNpbgC8ZQDOcSkHAWopKzQjJ1hI=', $response->headers()['Sec-WebSocket-Accept']);
+  }
+
+  #[Test]
+  public function translate_text_message() {
+    $response= $this->handle(new Request(new TestInput('POST', '/ws', [
+      'Sec-WebSocket-Version' => 9,
+      'Sec-WebSocket-Id'      => 123,
+      'Content-Type'          => 'text/plain',
+      'Content-Length'        => 4,
+    ], 'Test')));
+    Assert::equals(200, $response->status());
+    Assert::equals('text/event-stream', $response->headers()['Content-Type']);
+    Assert::matches('/10\r\ndata: Re: Test\n/', $response->output()->bytes());
+  }
+
+  #[Test]
+  public function translate_binary_message() {
+    $response= $this->handle(new Request(new TestInput('POST', '/ws', [
+      'Sec-WebSocket-Version' => 9,
+      'Sec-WebSocket-Id'      => 123,
+      'Content-Type'          => 'application/octet-stream',
+      'Content-Length'        => 2,
+    ], "\x47\x11")));
+    Assert::equals(200, $response->status());
+    Assert::equals('text/event-stream', $response->headers()['Content-Type']);
+    Assert::matches('/19\r\nevent: bytes\ndata: .{4}\n/', $response->output()->bytes());
   }
 
   #[Test]
