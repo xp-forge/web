29e4d7dd1a07e85d154627cd1b46cc1a162d895c
Timm Friebe <thekid@thekid.de>
2025-01-20 19:11:20 +0100

    Simplify test code by extracting common code to instance variable

--- src/test/php/web/unittest/server/WebsocketProtocolTest.class.php
+++ src/test/php/web/unittest/server/WebsocketProtocolTest.class.php
@@ -1,12 +1,13 @@
 <?php namespace web\unittest\server;
 
-use test\{Assert, Test};
+use test\{Assert, Before, Test};
 use util\Bytes;
 use web\unittest\Channel;
 use websocket\Listener;
 use xp\web\srv\WebsocketProtocol;
 
 class WebsocketProtocolTest {
+  private $noop;
 
   /**
    * Handles incoming payload with a given listener
@@ -27,6 +28,13 @@ class WebsocketProtocolTest {
     }
 
     return $socket->out;
+  }
+
+  #[Before]
+  public function noop() {
+    $this->noop= function($conn, $message) {
+      // NOOP
+    };
   }
 
   #[Test]
@@ -68,37 +76,25 @@ class WebsocketProtocolTest {
 
   #[Test]
   public function answers_ping_with_pong_automatically() {
-    $out= $this->handle(["\x89\x04", "Test"], function($conn, $message) {
-      // NOOP
-    });
-
+    $out= $this->handle(["\x89\x04", "Test"], $this->noop);
     Assert::equals(["\x8a\x04Test"], $out);
   }
 
   #[Test]
   public function default_close() {
-    $out= $this->handle(["\x88\x00"], function($conn, $message) {
-      // NOOP
-    });
-
+    $out= $this->handle(["\x88\x00"], $this->noop);
     Assert::equals(["\x88\x02\x03\xe8"], $out);
   }
 
   #[Test]
   public function answer_with_client_code_and_reason() {
-    $out= $this->handle(["\x88\x06", "\x03\xe8Test"], function($conn, $message) {
-      // NOOP
-    });
-
+    $out= $this->handle(["\x88\x06", "\x03\xe8Test"], $this->noop);
     Assert::equals(["\x88\x06\x03\xe8Test"], $out);
   }
 
   #[Test]
   public function protocol_error() {
-    $out= $this->handle(["\x88\x02", "\x03\xf7"], function($conn, $message) {
-      // NOOP
-    });
-
+    $out= $this->handle(["\x88\x02", "\x03\xf7"], $this->noop);
     Assert::equals(["\x88\x02\x03\xea"], $out);
   }
 }
