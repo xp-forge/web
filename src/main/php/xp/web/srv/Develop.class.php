c5414d9b51f40621b124bcd8d2a73f114a101492
Timm Friebe <thekid@thekid.de>
2025-02-02 10:32:14 +0100

    Extract worker logic into dedicated classes

--- src/main/php/xp/web/srv/Develop.class.php
+++ src/main/php/xp/web/srv/Develop.class.php
@@ -1,10 +1,8 @@
 <?php namespace xp\web\srv;
 
-use io\IOException;
-use lang\archive\ArchiveClassLoader;
-use lang\{ClassLoader, CommandLine, FileSystemClassLoader, Runtime, RuntimeOptions};
+use lang\ClassLoader;
+use peer\ServerSocket;
 use peer\server\AsyncServer;
-use peer\{Socket, ServerSocket};
 use util\cmd\Console;
 use web\{Application, Environment, Logging};
 use xp\web\Source;
@@ -27,34 +25,11 @@ class Develop extends Server {
     $application= (new Source($source, $environment))->application($args);
     $application->initialize();
 
-    // Start the development webserver in the background
     // PHP doesn't start with a nonexistant document root
     if (!$docroot->exists()) {
       $docroot= getcwd();
     }
-
-    // Inherit all currently loaded paths acceptable to bootstrapping
-    $include= '.'.PATH_SEPARATOR.PATH_SEPARATOR.'.';
-    foreach (ClassLoader::getLoaders() as $delegate) {
-      if ($delegate instanceof FileSystemClassLoader || $delegate instanceof ArchiveClassLoader) {
-        $include.= PATH_SEPARATOR.$delegate->path;
-      }
-    }
-
-    // Start `php -S`, the development webserver
-    $runtime= Runtime::getInstance();
-    $os= CommandLine::forName(PHP_OS);
-    $arguments= ['-S', '127.0.0.1:0', '-t', $docroot];
-    $cmd= $os->compose($runtime->getExecutable()->getFileName(), array_merge(
-      $arguments,
-      $runtime->startupOptions()
-        ->withSetting('user_dir', $docroot)
-        ->withSetting('include_path', $include)
-        ->withSetting('output_buffering', 0)
-        ->asArguments()
-      ,
-      [$runtime->bootstrapScript('web')]
-    ));
+    $workers= new Workers($docroot, ClassLoader::getLoaders());
 
     // Export environment
     putenv('DOCUMENT_ROOT='.$docroot);
@@ -65,25 +40,11 @@ class Develop extends Server {
     putenv('WEB_ARGS='.implode('|', $args));
     putenv('WEB_LOG='.$logging);
 
-    Console::writeLine("\e[33m@", nameof($this), "(HTTP @ `php ", implode(' ', $arguments), "`)\e[0m");
+    Console::writeLine("\e[33m@", nameof($this), "(HTTP @ `php -S [...] -t {$docroot}`)\e[0m");
     Console::writeLine("\e[1mServing {$profile}:", $application, $config, "\e[0m > ", $environment->logging()->target());
     Console::writeLine("\e[36m", str_repeat('═', 72), "\e[0m");
 
-    // Replace launching shell with PHP
-    if ('WINDOWS' !== $os->name()) $cmd= 'exec '.$cmd;
-    if (!($proc= proc_open($cmd, [STDIN, STDOUT, ['pipe', 'w']], $pipes, null, null, ['bypass_shell' => true]))) {
-      throw new IOException('Cannot execute `'.$runtime->getExecutable()->getFileName().'`');
-    }
-
-    // Parse `[...] PHP 8.3.15 Development Server (http://127.0.0.1:60922) started`
-    $line= fgets($pipes[2], 1024);
-    if (!preg_match('/\([a-z]+:\/\/([0-9.]+):([0-9]+)\)/', $line, $matches)) {
-      proc_terminate($proc, 2);
-      proc_close($proc);
-      throw new IOException('Cannot determine bound port: `'.trim($line).'`');
-    }
-    $backend= new Socket($matches[1], $matches[2]);
-
+    $backend= $workers->launch();
     Console::writeLinef(
       "\e[33;1m>\e[0m Server started: \e[35;4mhttp://%s:%d/\e[0m in %.3f seconds\n".
       "  %s - PID %d -> %d @ :%d; press Enter to exit\n",
@@ -92,40 +53,17 @@ class Develop extends Server {
       microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'],
       date('r'),
       getmypid(),
-      proc_get_status($proc)['pid'],
-      $matches[2],
+      $backend->pid(),
+      $backend->socket->port,
     );
 
     // Start the multiplex protocol in the foreground and forward requests
     $impl= new AsyncServer();
     $impl->listen(new ServerSocket($this->host, $this->port), Protocol::multiplex()
-      ->serving('http', new ForwardRequests($backend))
-      ->serving('websocket', new WebSocketProtocol(new ForwardMessages($backend)))
+      ->serving('http', new ForwardRequests($backend->socket))
+      ->serving('websocket', new WebSocketProtocol(new ForwardMessages($backend->socket)))
     );
     $impl->init();
     $impl->service();
-
-    // Inside `xp -supervise`, connect to signalling socket
-    if ($port= getenv('XP_SIGNAL')) {
-      $s= new Socket('127.0.0.1', $port);
-      $s->connect();
-      $s->canRead(null) && $s->read();
-      $s->close();
-    } else {
-      Console::read();
-      Console::write('> Shut down ');
-    }
-
-    // Wait for shutdown
-    proc_terminate($proc, 2);
-    do {
-      Console::write('.');
-      $status= proc_get_status($proc);
-      usleep(100 * 1000);
-    } while ($status['running']);
-
-    proc_close($proc);
-    Console::writeLine();
-    Console::writeLine("\e[33;1m>\e[0m Server stopped. (", date('r'), ')');
   }
 }
---
+++ src/main/php/xp/web/srv/Workers.class.php
@@ -0,0 +1,65 @@
+<?php namespace xp\web\srv;
+
+use io\{Path, IOException};
+use lang\archive\ArchiveClassLoader;
+use lang\{Runtime, RuntimeOptions, CommandLine, FileSystemClassLoader};
+use peer\Socket;
+
+/** Start PHP development webservers as background workers */
+class Workers {
+  private $commandLine;
+
+  /**
+   * Creates a new worker
+   *
+   * @param  string|io.Path $docroot
+   * @param  lang.IClassLoader[] $classLoaders
+   */
+  public function __construct($docroot, $classLoaders) {
+    $runtime= Runtime::getInstance();
+
+    // Inherit all currently loaded paths acceptable to bootstrapping
+    $include= '.'.PATH_SEPARATOR.PATH_SEPARATOR.'.';
+    foreach ($classLoaders as $delegate) {
+      if ($delegate instanceof FileSystemClassLoader || $delegate instanceof ArchiveClassLoader) {
+        $include.= PATH_SEPARATOR.$delegate->path;
+      }
+    }
+
+    // Replace launching shell with PHP on Un*x
+    $os= CommandLine::forName(PHP_OS);
+    $this->commandLine= $os->compose($runtime->getExecutable()->getFileName(), array_merge(
+      ['-S', '127.0.0.1:0', '-t', $docroot],
+      $runtime->startupOptions()
+        ->withSetting('user_dir', $docroot)
+        ->withSetting('include_path', $include)
+        ->withSetting('output_buffering', 0)
+        ->asArguments()
+      ,
+      [$runtime->bootstrapScript('web')]
+    ));
+    if ('WINDOWS' !== $os->name()) $this->commandLine= 'exec '.$this->commandLine;
+  }
+
+  /**
+   * Launches a worker and returns it.
+   * 
+   * @throws io.IOException
+   * @return xp.web.srv.Worker
+   */
+  public function launch() {
+    if (!($proc= proc_open($this->commandLine, [STDIN, STDOUT, ['pipe', 'w']], $pipes, null, null, ['bypass_shell' => true]))) {
+      throw new IOException('Cannot execute `'.$this->commandLine.'`');
+    }
+
+    // Parse `[...] PHP 8.3.15 Development Server (http://127.0.0.1:60922) started`
+    $line= fgets($pipes[2], 1024);
+    if (!preg_match('/\([a-z]+:\/\/([0-9.]+):([0-9]+)\)/', $line, $matches)) {
+      proc_terminate($proc, 2);
+      proc_close($proc);
+      throw new IOException('Cannot determine bound port: `'.trim($line).'`');
+    }
+
+    return new Worker($proc, new Socket($matches[1], (int)$matches[2]));
+  }
+}
