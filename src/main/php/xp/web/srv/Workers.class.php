5afae7c0e6048ac8139793e049a17f252dc8eedd
Timm Friebe <thekid@thekid.de>
2025-02-02 12:49:07 +0100

    Ensure launch() always selects a new free port

--- src/main/php/xp/web/srv/Workers.class.php
+++ src/main/php/xp/web/srv/Workers.class.php
@@ -30,20 +30,10 @@ class Workers {
       }
     }
 
-    // PHP 7.4 doesn't support ephemeral ports, see this commit which went into PHP 8.0:
-    // https://github.com/php/php-src/commit/a61a9fe9a0d63734136f995451a1fd35b0176292
-    if (PHP_VERSION_ID <= 80000) {
-      $s= stream_socket_server('tcp://127.0.0.1:0', $errno, $errstr, STREAM_SERVER_BIND);
-      $listen= stream_socket_get_name($s, false);
-      fclose($s);
-    } else {
-      $listen= '127.0.0.1:0';
-    }
-
     // Replace launching shell with PHP on Un*x
     $os= CommandLine::forName(PHP_OS);
     $this->commandLine= $os->compose($runtime->getExecutable()->getFileName(), array_merge(
-      ['-S', $listen, '-t', $docroot],
+      ['-S', '127.0.0.1:0', '-t', $docroot],
       $runtime->startupOptions()
         ->withSetting('user_dir', $docroot)
         ->withSetting('include_path', $include)
@@ -62,8 +52,20 @@ class Workers {
    * @return xp.web.srv.Worker
    */
   public function launch() {
-    if (!($proc= proc_open($this->commandLine, [STDIN, STDOUT, ['pipe', 'w']], $pipes, null, null, ['bypass_shell' => true]))) {
-      throw new IOException('Cannot execute `'.$this->commandLine.'`');
+
+    // PHP 7.4 doesn't support ephemeral ports, see this commit which went into PHP 8.0:
+    // https://github.com/php/php-src/commit/a61a9fe9a0d63734136f995451a1fd35b0176292
+    if (PHP_VERSION_ID <= 80000) {
+      $s= stream_socket_server('tcp://127.0.0.1:0', $errno, $errstr, STREAM_SERVER_BIND);
+      $listen= stream_socket_get_name($s, false);
+      fclose($s);
+      $commandLine= str_replace('127.0.0.1:0', $listen, $this->commandLine);
+    } else {
+      $commandLine= $this->commandLine;
+    }
+
+    if (!($proc= proc_open($commandLine, [STDIN, STDOUT, ['pipe', 'w']], $pipes, null, null, ['bypass_shell' => true]))) {
+      throw new IOException('Cannot execute `'.$commandLine.'`');
     }
 
     // Parse `[...] PHP 8.3.15 Development Server (http://127.0.0.1:60922) started`
