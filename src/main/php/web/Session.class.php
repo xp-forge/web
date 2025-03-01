48d593497ba4c07fe4f919b9d033c4f82b7e93d7
Timm Friebe <thekid@thekid.de>
2025-03-01 09:59:13 +0100

    Add session support

---
+++ src/main/php/web/Session.class.php
@@ -0,0 +1,26 @@
+<?php namespace web;
+
+abstract class Session {
+
+  public abstract function remainingTime();
+
+  public abstract function register($name, $value);
+
+  public abstract function value($name);
+
+  public abstract function remove($name);
+
+  public abstract function destroy();
+
+  public function cache($name, $provider, $ttl= null, $time= null) {
+    $time??= time();
+    if ($cached= $this->value($name)) {
+      [$stored, $value]= $cached;
+      if (null === $ttl || $stored < $time + $ttl) return $value;
+    }
+
+    $value= $provider();
+    $this->register($name, [$time, $value]);
+    return $value;
+  }
+}
