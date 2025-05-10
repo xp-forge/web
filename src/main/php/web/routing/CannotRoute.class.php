1649ca48c103c345b6ca927df38bfa2eff6db262
Timm Friebe <thekid@thekid.de>
2025-05-10 11:08:53 +0200

    Remove old routing API

--- src/main/php/web/routing/CannotRoute.class.php
+++
@@ -1,8 +0,0 @@
-<?php namespace web\routing;
-
-class CannotRoute extends \web\Error {
-
-  public function __construct($request) {
-    parent::__construct(404, 'Cannot route request to '.$request->uri()->path());
-  }
-}
